61ded124fa9bad84d3bf50dd9f8081512210a821
Cry0lab <dan-molina@outlook.com>
2019-03-24 20:39:15 -0500

    Cont...

--- Python/Wheel_of_Fortune.py
+++ Python/Wheel_of_Fortune.py
@@ -1,20 +1,37 @@
 import random
 
 wheel = []
-
+players = {}
 # Functions
 
 
-def wheel_maker(empty_wheel):
+def createPlayers():
+    i = 1
+    numberOfPlayers = int(input("How many players are going to be playing?"))
+    while i <= numberOfPlayers:
+        playerName = input("What is the name of Player " + str(i) + "?:  ")
+        print(playerName)
+        i += 1
+
+
+def wheelMaker(empty_wheel):
     for i in range(1, 20):
         empty_wheel.append((random.randint(1, 10))*100)
     empty_wheel.insert(random.randint(1, 15), "Lose a Turn")
+    empty_wheel.insert(random.randint(1, 15), "Bankrupt")
+
+
+def spin(phrase, wheel):
+    i = wheel[random.randint(0, len(wheel))]
+    print(i)
+
 
 # def phrase_picker:
-
+guessPhrase = "This is the phrase"
 
 # Generate the wheel
-wheel_maker(wheel)
+createPlayers()
+wheelMaker(wheel)
 print(wheel)
 
 # Play the game
@@ -28,5 +45,7 @@ while True:
     if x == '4':
         input("Quitter")
         break
+    elif x == '1':
+        spin(guessPhrase, wheel)
     else:
         print("Unregonized Command")
