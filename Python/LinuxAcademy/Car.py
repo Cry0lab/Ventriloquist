635cac9a31f143c0ced3c726894a919b33e8789b
Molina <Daniel_Molina1@Dellteam.com>
2019-09-05 17:12:47 -0500

    Updates

--- Python/LinuxAcademy/Car.py
+++ Python/LinuxAcademy/Car.py
@@ -1,5 +1,11 @@
 class Car:
+    """
+    Car models a car w/ tires and an engine
+    """
 
-    def _init_(self, engine, tires):
+    def __init__(self, engine, tires):
         self.engine = engine
         self.tires = tires
+
+    def description(self):
+        print(f"A car with an {self.engine} engine, and {self.tires} tires")
